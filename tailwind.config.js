4448c489485abb672eec9ba1fe57beca8583f86e
Minhazur rahman Nishat <cellsweb.com@gmail.com>
2023-08-09 04:32:04 +0600

    Init

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -11,6 +12,17 @@ module.exports = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        backgroundColor: {
+          dark: '#1F2937',
+          darker: '#111827',
+        },
+        textColor: {
+          dark: '#F9FAFB',
+        },
+        borderColor: {
+          dark: '#374151',
+        },
+        darkSelector: '.dark-mode',
       },
     },
   },
